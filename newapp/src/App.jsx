85824bc2fb3187eb3b49d48124fc5205a7720afd
kevin azuaje <kazuaje6@gmail.com>
2020-06-13 00:10:29 -0400

    compartiendo propiedades

--- newapp/src/App.jsx
+++ newapp/src/App.jsx
@@ -1,9 +1,39 @@
-import React, { Fragment } from "react";
+import React from "react";
 
 import "./styles/styles.scss";
 
 import Curse from "./components/curse";
 function App() {
+  const cursos = [
+    {
+      title: "sprint",
+      price: "40",
+      cimage:
+        "https://images.pexels.com/photos/40751/running-runner-long-distance-fitness-40751.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      teacher: "kevin azuaje",
+      timage:
+        "https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-1/c0.0.320.320a/p320x320/97062444_256669122377752_5522323495834353664_o.jpg?_nc_cat=105&_nc_sid=dbb9e7&_nc_ohc=galmLzM818YAX_s-20f&_nc_ht=scontent-mia3-2.xx&oh=20c23e187fcb771d9a358dc18922a51e&oe=5F0A4599",
+    },
+    {
+      title: "swim",
+      price: "70",
+      cimage:
+        "https://images.pexels.com/photos/565999/pexels-photo-565999.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      timage:
+        "https://images.pexels.com/photos/3754637/pexels-photo-3754637.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      teacher: "teacher1",
+    },
+    {
+      title: "boxeo",
+      price: "20",
+      cimage:
+        "https://images.pexels.com/photos/163403/box-sport-men-training-163403.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      timage:
+        "https://images.pexels.com/photos/935965/pexels-photo-935965.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      teacher: "teacher2",
+    },
+  ];
+
   return (
     <div>
       <div className="main-banner img-container" id="main-banner">
@@ -24,9 +54,17 @@ function App() {
           </div>
         </div>
       </div>
-      <div>
-        <Curse />
-      </div>
+      <div></div>
+
+      {cursos.map((c) => (
+        <Curse
+          title={c.title}
+          teacher={c.teacher}
+          timage={c.timage}
+          price={c.price}
+          cimage={c.cimage}
+        />
+      ))}
     </div>
   );
 }
--- newapp/src/components/curse.jsx
+++ newapp/src/components/curse.jsx
@@ -1,52 +1,31 @@
 import React, { Component } from "react";
-import PropType from "prop-types";
+// import PropType from "prop-types";
 
 export default class curse extends Component {
   render() {
-    const mayor = (age) => age > 18;
-
-    const person = {
-      name: "kevin",
-      sname: "azuaje",
-      age: 21,
-      image:
-        "https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-1/c0.0.320.320a/p320x320/97062444_256669122377752_5522323495834353664_o.jpg?_nc_cat=105&_nc_sid=dbb9e7&_nc_ohc=galmLzM818YAX_s-20f&_nc_ht=scontent-mia3-2.xx&oh=20c23e187fcb771d9a358dc18922a51e&oe=5F0A4599",
-    };
-
-    const sport = {
-      name: "Sprint",
-      price: 40,
-      image:
-        "https://images.pexels.com/photos/40751/running-runner-long-distance-fitness-40751.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    };
-
     return (
       <div>
         <article class="card">
           <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-            <img src={sport.image} alt="city" />
+            <img src={this.props.cimage} alt="Imagen del curso" />
           </div>
           <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-            <h2 className="t5 s-mb-2 s-center">{sport.name}</h2>
+            <h2 className="t5 s-mb-2 s-center">{this.props.title}</h2>
             <div className="s-mb-2 s-main-center">
               <div className="card__teacher s-cross-center">
                 <div className="card__avatar s-mr-1">
                   <div className="circle img-container">
-                    <img src={person.image} alt="" />
+                    <img src={this.props.timage} alt="teacher" />
                   </div>
                 </div>
                 <span className="Small">
-                  {mayor(person.age) ? (
-                    <h3>{person.name}</h3>
-                  ) : (
-                    <p>No tengo permitido el ingreso</p>
-                  )}
+                  <h3>{this.props.teacher}</h3>
                 </span>
               </div>
             </div>
             <div className="s-main-center">
               <a className="button--ghost-alert button--tiny" href="#">
-                Buy {sport.price}$ duration
+                Buy {this.props.price}$
               </a>
             </div>
           </div>
