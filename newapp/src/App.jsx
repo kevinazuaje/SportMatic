6ccde0bcf68b1f6ae1fc5222bce6de1b7e871f9a
kevin azuaje <kazuaje6@gmail.com>
2020-06-13 09:24:08 -0400

    topbar

--- newapp/src/App.jsx
+++ newapp/src/App.jsx
@@ -3,9 +3,11 @@ import Form from "./components/Form";
 import "./styles/styles.scss";
 import Banner from "./components/Banner";
 import Curses from "./components/Curses";
+import TopBar from "./components/TopBar";
 function App() {
   return (
     <div>
+      <TopBar />
       <Banner />
       <Form />
       <Curses />
---
+++ newapp/src/components/TopBar.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export default class TopBar extends Component {
+  render() {
+    return (
+      <div>
+        <div class="topbar">
+          <div class="ed-grid">
+            <h2>contenido</h2>
+          </div>
+        </div>
+
+        <div class="topbar">
+          <div class="ed-grid m-grid-2">
+            <p class="s-mb-1 m-mb-0 s-center m-left"></p>
+            <a
+              class="button--cta button--tiny s-to-center m-to-right"
+              href="#"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
